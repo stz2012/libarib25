8c07c2c1accc2a0bbbeab89b9306a241b424b6f8
Takuya Sawada <takuya@tuntunkun.com>
2017-06-09 11:54:38 +0900

    ldconfig コマンドが存在しないシステムに対応

--- cmake/PostInstall.cmake
+++ cmake/PostInstall.cmake
@@ -1,6 +1,7 @@
-message(STATUS "Running: ldconfig")
-
-execute_process(COMMAND ${CMD_LDCONFIG} RESULT_VARIABLE ldconfig_result)
-if (NOT ldconfig_result EQUAL 0)
-	message(WARNING "ldconfig failed")
+if(LDCONFIG_EXECUTABLE)
+	message(STATUS "Running: ldconfig")
+	execute_process(COMMAND ${LDCONFIG_EXECUTABLE} RESULT_VARIABLE ldconfig_result)
+	if (NOT ldconfig_result EQUAL 0)
+		message(WARNING "ldconfig failed")
+	endif()
 endif()
